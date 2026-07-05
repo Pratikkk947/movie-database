0b5cb3cec5dfe886d13e9355fa0ba3fcd6057f3e
Pratik Shah <pratik@Pratiks-MacBook-Air-3.local>
2026-07-05 22:46:02 +0545

    feat: added sample movie data

---
+++ src/data/movies.js
@@ -0,0 +1,39 @@
+const movies = [
+    {
+      id: 1,
+      title: "Inception",
+      genre: "Sci-Fi",
+      year: 2010,
+      rating: 8.8,
+    },
+    {
+      id: 2,
+      title: "Interstellar",
+      genre: "Adventure",
+      year: 2014,
+      rating: 8.7,
+    },
+    {
+      id: 3,
+      title: "The Batman",
+      genre: "Action",
+      year: 2022,
+      rating: 7.9,
+    },
+    {
+      id: 4,
+      title: "Morbius",
+      genre: "Action",
+      year: 2022,
+      rating: 5.1,
+    },
+    {
+      id: 5,
+      title: "Catwoman",
+      genre: "Fantasy",
+      year: 2004,
+      rating: 3.4,
+    },
+  ];
+  
+  export default movies;
