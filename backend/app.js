8d03f16f2523d3b3f30078a01ab4ab8b956f7d2e
Pratik Shah <pratik@Pratiks-MacBook-Air-3.local>
2026-07-14 10:50:29 +0545

    feat: add GET /api/movies endpoint

--- backend/app.js
+++ backend/app.js
@@ -18,9 +18,14 @@ app.get("/", (req, res) => {
 });
 
 // Get All Movies
+// GET all movies
 app.get("/api/movies", (req, res) => {
-  return res.json(movies);
-});
+    return res.status(200).json({
+      success: true,
+      count: movies.length,
+      data: movies,
+    });
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
